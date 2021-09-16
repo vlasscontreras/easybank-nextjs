6569e5f6be7dad9ae736afb912668b5779c98252
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-09-16 14:58:08 -0600

    🌐 Add custom locales

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['source.unsplash.com']
-  }
-}
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+};
