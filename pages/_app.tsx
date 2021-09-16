c8317cb11df96c06c55918cf796e3ba29d78994d
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-09-16 12:22:51 -0600

    🚸 Add a loader indicator when loading routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress';
+import 'node_modules/nprogress/nprogress.css';
 import Layout from 'components/layout/Layout';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start()
+    };
+
+    const handleStop = () => {
+      NProgress.done()
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    }
+  }, [router]);
+
   return <Layout><Component {...pageProps} /></Layout>
 }
 export default MyApp
